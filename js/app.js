d8553ac4c05566d7d085a4e6059e70066fcd7fcf
Cintia Tahirih <tahysluana@gmail.com>
2018-02-16 01:39:08 -0500

    redefine estructura

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
-var ready = function() {
-  var toggle = document.querySelector('.main-menu-toggle');
+/* jslint browser:true */
+$(document).ready(function() {
   
-  toggle.onclick = function() {
-    var menu = document.querySelector('.main-navigation');
-    var toggle = document.querySelector('.main-menu-toggle');
-   
-    menu.classList.toggle('menu-show');
-    toggle.classList.toggle('on');
-  };
-};
-  
-document.addEventListener('DOMContentLoaded', ready);
-  
+});
