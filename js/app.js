7ccc889bc3f999ea558aef828f8669d15f58e123
Cintia Tahirih <tahysluana@gmail.com>
2018-02-17 01:31:08 -0500

    añade evento scroll al header

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function() {
+  var menuOffset = $('#header').offset();
+  
+  $(document).on('scroll', function() {
+    var scrollTop = $(document).scrollTop();
+    if (scrollTop > menuOffset.top) {
+      $('#menu-nav').addClass('menu-white');
+      $('#menu-nav').removeClass('menu-transparent');
+    } else {
+      $('#menu-nav').removeClass('menu-white');
+      $('#menu-nav').addClass('menu-transparent');
+    }
+  });
+});
