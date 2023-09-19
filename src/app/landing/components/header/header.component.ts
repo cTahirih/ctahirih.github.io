dd413d97e72aadbe9c5ffb7fd41dc31d27db3fb8
cjalirip <cintiajaliri@innovacxion.com>
2023-09-19 14:58:34 -0500

    update docs file

--- src/app/landing/components/header/header.component.ts
+++ src/app/landing/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,31 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class HeaderComponent {
   @Output() isOnClickButton = new EventEmitter<boolean>();
 
+  constructor(
+    private gtm: GoogleTagManagerService
+  ) {
+
+  }
+
+  onClickLinksIcon(socialMedia: string) {
+    this.sendTag( {
+      section: 'headerLanding',
+      socialMedia
+    })
+  }
+
   scrollAboutSection() {
+    this.sendTag( {
+      viewAboutSection: true,
+      section: 'headerLanding',
+    })
     this.isOnClickButton.emit(true);
+  }
+
+  sendTag(content: { [key: string]: any; }) {
+    this.gtm.pushTag({
+      event: 'viewAbout',
+      content
+    });
   }
 }
